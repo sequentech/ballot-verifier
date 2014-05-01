78de7c84f03fa25bd7c24386d85578197827150e
Findeton <felrobelv@gmail.com>
2014-05-01 23:16:22 +0200

    setup works but needs to compile linux and be converted to a makefile

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 # (POSIX shell syntax)
 
+#Create log
+[ -f log ] && { echo "" > log; } || { touch log; }
 
 #Setup some environment variables
 INSTALLDIR=$(pwd)
-type svn >/dev/null 2>&1 || { echo >&2 "Fail! subversion not found, now installing svn..."; sudo apt-get install subversion;}
-type git >/dev/null 2>&1 || { echo >&2 "Fail! git not found, now installing  git..."; sudo apt-get install git;}
+echo "Setup some environment variables" >> $INSTALLDIR/log
+
+#Check if some needed programs are installed and install them if needed (will ask for password)
+type svn >/dev/null 2>&1 || { echo >&2 "Fail! subversion not found, now installing svn..."; sudo apt-get install subversion; }
+type git >/dev/null 2>&1 || { echo >&2 "Fail! git not found, now installing  git..."; sudo apt-get install git; }
+echo "" >> $INSTALLDIR/log
+
+#or1ksim
+echo "getting the svn of or1ksim" >> $INSTALLDIR/log
 svn co http://opencores.org/ocsvn/openrisc/openrisc/trunk/or1ksim
-echo "Finished svn co"
 cd or1ksim
 mkdir builddir_or1ksim
 cd builddir_or1ksim
 ../configure --target=or32-elf --prefix=/opt/or1ksim
 make all
 echo "Please enter your password to install the or1ksim OpenRISC simulator"
+echo "installing or1ksim" >> $INSTALLDIR/log
 sudo make install
 export PATH=/opt/or1ksim/bin:$PATH
 grep -Fxq "export PATH=/opt/or1ksim/bin:\$PATH" $HOME/.bashrc || {
@@ -22,8 +31,76 @@ source $HOME/.bashrc
 }
 cd $INSTALLDIR
 
+#Newlib toolchain (or1k-elf) 
+echo "Newlib toolchain (or1k-elf)" >> $INSTALLDIR/log
+git clone git://github.com/openrisc/or1k-src.git
+git clone git://github.com/openrisc/or1k-gcc.git
+[ -d $INSTALLDIR/bld-or1k-src ] || { mkdir bld-or1k-src; }
+[ -d $INSTALLDIR/bld-or1k-gcc ] || { mkdir bld-or1k-gcc; }
+grep -Fxq "export PATH=/opt/or1k-toolchain/bin:\$PATH" $HOME/.bashrc || {
+echo "export PATH=/opt/or1k-toolchain/bin:\$PATH" >> $HOME/.bashrc
+source $HOME/.bashrc
+}
+[ -d /opt/or1k-toolchain ] || { sudo mkdir /opt/or1k-toolchain; }
+sudo chown $(whoami):$(id -g -n $(whoami)) -R /opt/or1k-toolchain
+
+#Build the first set of tools, binutils etc (for Newlib toolchain)
+echo "Build the first set of tools, binutils etc (for Newlib toolchain)" >> $INSTALLDIR/log 
+sudo apt-get install libgmp-dev libmpfr-dev libmpc-dev bison flex texinfo libncurses5-dev
+cd bld-or1k-src
+echo "Configure..." >> $INSTALLDIR/log 
+../or1k-src/configure --target=or1k-elf --prefix=/opt/or1k-toolchain --enable-shared \
+--disable-itcl --disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda \
+--disable-sid --disable-sim --disable-gdb --with-sysroot --disable-newlib --disable-libgloss
+echo "Building..." >> $INSTALLDIR/log 
+make
+echo "Installing..." >> $INSTALLDIR/log 
+make install
+
+#Build gcc 
+echo "Build gcc" >> $INSTALLDIR/log
+cd ../bld-or1k-gcc
+../or1k-gcc/configure --target=or1k-elf --prefix=/opt/or1k-toolchain --enable-languages=c \
+--disable-shared --disable-libssp
+make
+make install
+
+#Build newlib and gdb (without or1ksim in this case)  (for Newlib toolchain) 
+echo "Build newlib and gdb (without or1ksim in this case)  (for Newlib toolchain) " >> $INSTALLDIR/log
+cd ..
+rm -rf bld-or1k-src
+mkdir bld-or1k-src  
+cd bld-or1k-src
+../or1k-src/configure --target=or1k-elf --prefix=/opt/or1k-toolchain --enable-shared --disable-itcl \
+--disable-tk --disable-tcl --disable-winsup --disable-gdbtk --disable-libgui --disable-rda --disable-sid \
+--enable-sim --disable-or1ksim \
+--enable-gdb  --with-sysroot --enable-newlib --enable-libgloss
+make
+make install
+
+#Build gcc again, this time with newlib (for Newlib toolchain) 
+echo "Build gcc again, this time with newlib (for Newlib toolchain) " >> $INSTALLDIR/log
+cd ..
+rm -rf bld-or1k-gcc
+mkdir bld-or1k-gcc
+cd bld-or1k-gcc
+
+../or1k-gcc/configure --target=or1k-elf --prefix=/opt/or1k-toolchain --enable-languages=c,c++ --disable-shared \
+--disable-libssp --with-newlib
+make
+make install
+
+
+echo "SUCCESSS! " >> $INSTALLDIR/log
+
+#Linux (uClibc) toolchain (or1k-linux-uclibc) 
+
+	
+
 exit
 
+
+#this is the skeleton of what will probably used in the future to control options in this script, but for now it's not used at all.
 # Usage info
 show_help() {
 echo "Usage: ${0##*/} [-hv] [-f OUTFILE] [FILE]...
