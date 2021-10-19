68d48e00b834ce0ec943b76804ef76c74ffebd9c
Eduardo Robles Elvira <edulix@gmail.com>
2021-10-19 17:47:10 +0200

    compilation works now in nix for mac os x.. didn't do any unit testing yet

--- flake.nix
+++ flake.nix
@@ -1,21 +1,93 @@
+/**
+ * This file is part of agora-airgap.
+ * Copyright (C) 2014-2021  Agora Voting SL <agora@agoravoting.com>
+
+ * agora-airgap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+
+ * agora-airgap  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with agora-airgap.  If not, see <http://www.gnu.org/licenses/>.
+**/
 {
-  description = "Flake for agora-verifier, a cast-as-intended verifier for nVotes platform";
+  description = "Flake for agora-airgap, a cast-as-intended verifier for nVotes platform";
 
-  inputs.nixpkgs.linux.url = github:NixOS/nixpkgs/nixos-21.05;
-  inputs.nixpkgs.darwin.url = github:NixOS/nixpkgs/nixpkgs-21.05-darwin;
+  # input
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
+  # output function of this flake
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let 
+          # pkgs is just the nix packages
+          pkgs = nixpkgs.legacyPackages.${system};
 
-    packages.x86_64-darwin.agora-airgap = 
-      with nixpkgs.darwin { system = "x86_64-darwin"; };
-      stdenv.mkDerivation {
-        name = "hello";
-        src = self;
-        buildPhase = "gcc -o hello ./hello.c";
-        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
-      };
+          # Static GMP with C++ support
+          gmpCustom = 
+            pkgs
+              .gmp
+              .override {
+                withStatic = true;
+                cxx = true;
+              };
 
-    defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.hello;
+          # static wxmac/wxGTK30
+          wxCustom = 
+            (if system == "x86_64-darwin" then pkgs.wxmac else pkgs.wxGTK30);
 
-  };
+          # static curl with idn support
+          curlCustom = 
+            (pkgs.curl
+              .override {
+                idnSupport = true;
+                sslSupport = true;
+              });
+
+        # resulting packages of the flake
+        in rec {
+          # Derivation for agora-airgap
+          packages.agora-airgap = pkgs.stdenv.mkDerivation {
+            name = "agora-airgap";
+            version = "4.0.2";
+            src = self;
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [
+                pkgs.gnumake
+                pkgs.gnum4
+                pkgs.rapidjson
+                # the overlayed libraries
+                gmpCustom
+                wxCustom
+                curlCustom
+              ] 
+              ++ nixpkgs.lib.optionals (system == "x86_64-darwin") [
+                pkgs.pkgs.darwin.apple_sdk.frameworks.IOKit
+                pkgs.pkgs.darwin.apple_sdk.frameworks.Carbon
+                pkgs.pkgs.darwin.apple_sdk.frameworks.Cocoa
+              ];
+            buildPhase = ''
+              cd src && make gui
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              [ -f x64/agora-audit ] && mv x64/agora-audit $out/bin
+              echo $?
+            '';
+          };
+          # agora-airgap is the default package
+          defaultPackage = packages.agora-airgap;
+
+          # configure the dev shell
+          devShell = pkgs.mkShell { 
+            buildInputs = packages.agora-airgap.buildInputs; 
+          };
+        }
+    );
 }
